4749997b840f4e9ef22eb95a9cd8bd46ad1566d9
Muhammad Arslan <mar@axew.io>
2020-09-17 19:03:07 +0500

    Updatedcode

--- MicroRabbit/MicroRabbit.Transfer.Api/Startup.cs
+++ MicroRabbit/MicroRabbit.Transfer.Api/Startup.cs
@@ -2,10 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MediatR;
+using MicroRabbit.Banking.Data.Context;
+using MicroRabbit.Infra.IoC;
+using MicroRabbit.Transfer.Data.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,8 +31,29 @@ namespace MicroRabbit.Transfer.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-        }
 
+            RegisterServices(services);
+
+            //  services.AddMediatR(typeof(RabbitMQBus).Assembly);
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+                {
+                    Title = "Transfer Microservice",
+                    Version = "v1",
+                    Description = "Microservice",
+                });
+            });
+            services.AddMediatR(typeof(Startup));
+            services.AddDbContext<TransferDbContext>(options =>
+
+                options.UseSqlServer(Configuration.GetConnectionString("TransferDBConnection"))
+            );
+        }
+        private void RegisterServices(IServiceCollection services)
+        {
+            DependencyContainer.RegisterServices(services);
+        }
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
@@ -46,6 +72,8 @@ namespace MicroRabbit.Transfer.Api
             {
                 endpoints.MapControllers();
             });
+            app.UseSwagger();
+            app.UseSwaggerUI(options => options.SwaggerEndpoint("/swagger/v1/swagger.json", "Transfer Microservice"));
         }
     }
 }
